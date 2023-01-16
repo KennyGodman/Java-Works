339397bc8d26b13aced4f88e18d9f3c6efd2632a
Kehinde <kennydaniels138@gmail.com>
2023-01-16 08:56:00 +0100

    fresh commit

--- src/Chapter7/simulationwork/Animal.java
+++ src/Chapter7/simulationwork/Animal.java
@@ -1,21 +1,21 @@
-package Chapter7.simulationwork;
-
-import javax.swing.plaf.basic.BasicIconFactory;
-import java.math.BigDecimal;
-
-public abstract class Animal {
-    private int position = BigDecimal.ONE.intValue();
-
-    public int getPosition() {
-        return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
-    }
-    public abstract int move(){
-        return 0;
-
-    }
-
-}
+////package Chapter7.simulationwork;
+//
+//import javax.swing.plaf.basic.BasicIconFactory;
+//import java.math.BigDecimal;
+//
+//public abstract class Animal {
+//    private int position = BigDecimal.ONE.intValue();
+//
+//    public int getPosition() {
+//        return position;
+//    }
+//
+//    public void setPosition(int position) {
+//        this.position = position;
+//    }
+//    public abstract int move(){
+//        return 0;
+//
+//    }
+//
+//}
